b789c402712db68978f120012daf7b369a46da55
Hachem <HackSek@users.noreply.github.com>
2018-02-08 22:13:29 +0000

    first part of the commenting phase

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let setTime = function() {
 
+  // creating an instance of the Date object and assigning it to the moment variable
   const moment = new Date()
+
   const seconds = moment.getSeconds()
+
   const secondsHand = document.querySelector('.seconds')
+
   const secondsToDegrees = ((seconds / 60) * 360) + 90
+
   secondsHand.style.transform = `rotate(${secondsToDegrees}deg)`
 
+
   const minutes = moment.getMinutes()
+
   const minutesHand = document.querySelector('.minutes')
+
   const minutesToDegrees = ((minutes / 60) * 360) + 90
+
   minutesHand.style.transform = `rotate(${minutesToDegrees}deg)`
 
 
   const hours = moment.getHours()
+
   const hoursHand = document.querySelector('.hours')
+
   const hoursToDegrees = ((hours / 12) * 360) + 90
+
   hoursHand.style.transform = `rotate(${hoursToDegrees}deg)`
 
 }
